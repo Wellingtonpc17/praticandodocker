c6b2e1c2d27d3bb88b970c6af6ec4f520bca57ad
wagnermarques <wagnerdocri@gmail.com>
2026-03-05 20:10:35 -0300

    testando php

--- meufrontend/index.php
+++ meufrontend/index.php
@@ -1,11 +1,18 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
     <h1>MEU HTML</h1>
+    <?php
+        $nome = "Claude Shanon";
+        echo "<h2>Olá $nome !!!</h2>"
+    ?>
 </body>
+
 </html>
